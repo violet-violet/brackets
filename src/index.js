28512653b5c37816f8fe2fc7ae8920a1f74e4f03
E.Demidovskaia <violet7693@gmail.com>
2020-10-22 22:23:25 +0300

    feat: add solution fot the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brackets) {
+  let bracketsString = '';
+
+  brackets.forEach(item => {
+      item.forEach(el => {
+          bracketsString += el;
+      });
+  });
+
+  let stack = [];
+
+  for (let bracket of str) {
+      let bracketsIndex = bracketsString.indexOf(bracket, 0);
+
+      if (bracketsIndex === -1) {
+          continue;
+      }
+
+      if (bracketsString.indexOf(bracket, 0) === bracketsString.lastIndexOf(bracket)) {
+
+          if(bracketsIndex % 2 === 0) {
+              stack.push(bracketsIndex + 1);
+          } else {
+              if(stack.pop() !== bracketsIndex) {
+                  return false;
+              }
+          }
+
+      } else {
+          if (stack[stack.length - 1] === bracketsIndex) {
+              stack.pop();
+          } else {
+              stack.push(bracketsIndex);
+          }
+      }
+  }    
+  return stack.length === 0;
 }
--- src/script.js
+++ src/script.js
@@ -1,51 +1,83 @@
 function check(str, brackets) {
-    console.log(`Входной массив со скобками для проверки: ${brackets}`);
+    let bracketsString = '';
 
-    // let brackets = "[]{}()<>";
-
-    // let brackets1 = [];
-
-    console.log(brackets);
-
-    // brackets.forEach(item => {
-    // });
-
-    let arr = brackets.reduce((acc, item) => {
-        acc = acc.concat(item);
-        console.log(acc);
-    }, []);
-
-
+    brackets.forEach(item => {
+        item.forEach(el => {
+            bracketsString += el;
+        });
+    });
+    // console.log(bracketsString);
 
     let stack = [];
+
     for (let bracket of str) {
-        let bracketsIndex = brackets.indexOf(bracket, 0);
-        console.log(brackets.indexOf(bracket));
+        // console.log(`Проверяемая скобка = ${bracket}`);
+
+        let bracketsIndex = bracketsString.indexOf(bracket, 0);
+        // console.log(`Индекс проверяемой скобки = ${bracketsIndex}`);
 
         if (bracketsIndex === -1) {
             continue;
         }
 
-  	    if(bracketsIndex % 2 === 0) {
-            stack.push(bracketsIndex + 1);
+        if (bracketsString.indexOf(bracket, 0) === bracketsString.lastIndexOf(bracket)) {
+            // console.log("----СВЕРХУ ОБЫЧНАЯ СКОБКА----");
+            // console.log(`Обычная потому что: ind с начала равен ${bracketsString.indexOf(bracket, 0)}`);
+            // console.log(`Обычная потому что: ind с конца равен ${bracketsString.lastIndexOf(bracket)}`);
+
+
+            if(bracketsIndex % 2 === 0) {
+                stack.push(bracketsIndex + 1);
+            } else {
+                if(stack.pop() !== bracketsIndex) {
+                    return false;
+                }
+            }
+
         } else {
-            if(stack.pop() !== bracketsIndex) {
-                return false;
+            // console.log("----СВЕРХУ ОДИНАКОВАЯ СКОБКА----");
+            // console.log(`Обычная потому что: ind с начала равен ${bracketsString.indexOf(bracket, 0)}`);
+            // console.log(`Обычная потому что: ind с конца равен ${bracketsString.lastIndexOf(bracket)}`);
+
+            if (stack[stack.length - 1] === bracketsIndex) {
+                stack.pop();
+            } else {
+                stack.push(bracketsIndex);
             }
         }
     }    
     return stack.length === 0
 }
 
-const config1 = [['(', ')']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 
-// console.log(`Получили: ${check('()', config1)}, а должно быть: true`); 
-// console.log(`Получили: ${check('())(', config1)}, а должно быть: false`);
-// console.log(`Получили: ${check('([{}])', config3)}, а должно быть: true`);
-// console.log(`Получили: ${check('[]][[]', config3)}, а должно быть: false`);  
+console.log(`Получили: ${check('||', config4)}, а должно быть: true`); 
+console.log(`Получили: ${check('|()|', config5)}, а должно быть: true`);
+console.log(`Получили: ${check('|()|(||)||', config5)}, а должно быть: true`);
+console.log(`Получили: ${check('111115611111111222288888822225577877778775555666677777777776622222', config6)}, а должно быть: true`);  
+console.log(`Получили: ${check('111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222', config6)}, а должно быть: true`);  
+console.log(`Получили: ${check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())', config7)}, а должно быть: true`);  
+console.log(`Получили: ${check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))', config7)}, а должно быть: true`);  
 
-check('()', config1);
-check('())(', config1);
-check('([{}])', config3);
-check('[]][[]', config3);
+
+// 8
+// (check('||', config4), true)
+
+// 9
+// (check('|()|', config5), true)
+// 11
+// (check('|()|(||)||', config5), true)
+// 12
+// assert.equal(check('111115611111111222288888822225577877778775555666677777777776622222', config6), true);
+
+// 15
+// assert.equal(check('111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222', config6), true);
+
+// 19
+// assert.equal(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())', config7), true);
+
+// 20
+// assert.equal(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))', config7), true);
