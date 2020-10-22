24e903c5ee9438123870ee2017061a2f9c9bf6ca
E. Demidovskaia <violet7693@yandex.ru>
2020-10-22 11:59:24 +0300

    feat: add the main algorithm

--- src/script.js
+++ src/script.js
@@ -1,21 +1,51 @@
-function check(str, bracketsConfig) {
-    let brackets = "[]{}()<>";
+function check(str, brackets) {
+    console.log(`Входной массив со скобками для проверки: ${brackets}`);
+
+    // let brackets = "[]{}()<>";
+
+    // let brackets1 = [];
+
+    console.log(brackets);
+
+    // brackets.forEach(item => {
+    // });
+
+    let arr = brackets.reduce((acc, item) => {
+        acc = acc.concat(item);
+        console.log(acc);
+    }, []);
+
+
+
     let stack = [];
+    for (let bracket of str) {
+        let bracketsIndex = brackets.indexOf(bracket, 0);
+        console.log(brackets.indexOf(bracket));
 
-    for (let bracket of input) {
-        let bracketsIndex = brackets.indexOf(bracket);
+        if (bracketsIndex === -1) {
+            continue;
+        }
 
-  	// record order on the stack
-    // check this record to make sure the brackets are all balanced
-    }
-    return true ? true : false;
-
+  	    if(bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex + 1);
+        } else {
+            if(stack.pop() !== bracketsIndex) {
+                return false;
+            }
+        }
+    }    
+    return stack.length === 0
 }
 
 const config1 = [['(', ')']];
 const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
 
-console.log(`Получили: ${check('()', config1)}, а должно быть: true`);
-console.log(`Получили: ${check('())(', config1)}, а должно быть: false`);
-console.log(`Получили: ${check('([{}])', config3)}, а должно быть: true`);
-console.log(`Получили: ${check('[]][[]', config3)}, а должно быть: false`);  
+// console.log(`Получили: ${check('()', config1)}, а должно быть: true`); 
+// console.log(`Получили: ${check('())(', config1)}, а должно быть: false`);
+// console.log(`Получили: ${check('([{}])', config3)}, а должно быть: true`);
+// console.log(`Получили: ${check('[]][[]', config3)}, а должно быть: false`);  
+
+check('()', config1);
+check('())(', config1);
+check('([{}])', config3);
+check('[]][[]', config3);
